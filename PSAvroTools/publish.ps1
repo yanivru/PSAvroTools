4c51be489148e3c4c883dfa1f2c7ea221d2c2a1b
yanivru <yaniv.ru@gmail.com>
2022-01-11 01:19:09 +0200

    Update publish.ps1

--- PSAvroTools/publish.ps1
+++ PSAvroTools/publish.ps1
@@ -1,3 +1,3 @@
 ﻿dotnet publish -c Release
-copy .\bin\Release\netstandard2.0\publish\*.* $HOME\Documents\WindowsPowerShell\Modules\AvroTools\
+rem copy .\bin\Release\netstandard2.0\publish\*.* $HOME\Documents\WindowsPowerShell\Modules\AvroTools\
 Publish-Module -Name AvroTools -NuGetApiKey $key -RequiredVersion $version
