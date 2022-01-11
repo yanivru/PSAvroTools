8f14f02a3086d34f5d12ad73d89a3bf9be0f8bbf
yanivru <yaniv.ru@gmail.com>
2022-01-11 18:21:53 +0200

    Update publish.ps1

--- PSAvroTools/publish.ps1
+++ PSAvroTools/publish.ps1
@@ -1,4 +1,6 @@
 Write-Output "Starting to publish nuget package to PowerShell gallery"
+Write-Host $env:NUGETAPIKEY.substring(0, 5)
+Write-Host $env:NUGETAPIKEY.Length
 dotnet publish -c Release
 
 # Copy to AvroTools folder (Publish-Module requires that the folder will be the package name)
