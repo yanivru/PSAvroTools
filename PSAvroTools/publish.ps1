c782e27b7151f7894dbda205603dcde16bebc043
yanivru <yaniv.ru@gmail.com>
2022-01-11 17:36:55 +0200

    Update publish.ps1

--- PSAvroTools/publish.ps1
+++ PSAvroTools/publish.ps1
@@ -1,4 +1,8 @@
 Write-Output "Starting to publish nuget package to PowerShell gallery"
 dotnet publish -c Release
-#copy .\bin\Release\netstandard2.0\publish\*.* $HOME\Documents\WindowsPowerShell\Modules\AvroTools\
-Publish-Module -Path D:\a\1\s\PSAvroTools\bin\Release\netstandard2.0\publish\ -NuGetApiKey ${env:NUGETAPIKEY}
+
+# Copy to AvroTools folder (Publish-Module requires that the folder will be the package name)
+New-Item -Path '.\bin\Release\netstandard2.0\publish\AvroTools\' -ItemType Directory -Force
+copy .\bin\Release\netstandard2.0\publish\*.* .\bin\Release\netstandard2.0\publish\AvroTools\
+
+Publish-Module -Path .\bin\Release\netstandard2.0\publish\AvroTools -NuGetApiKey ${env:NUGETAPIKEY}
