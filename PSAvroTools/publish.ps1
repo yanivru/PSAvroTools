0ff4b75f053dc285262f3df55d929fbde08fbcf9
yanivru <yaniv.ru@gmail.com>
2025-05-24 20:34:49 +0300

    Fix publish ps1 to work from root dir

--- PSAvroTools/publish.ps1
+++ PSAvroTools/publish.ps1
@@ -3,7 +3,7 @@ Write-Host $env:NUGETAPIKEY.Length
 dotnet publish -c Release
 
 # Copy to AvroTools folder (Publish-Module requires that the folder will be the package name)
-New-Item -Path '.\bin\Release\netstandard2.0\publish\AvroTools\' -ItemType Directory -Force
-copy .\bin\Release\netstandard2.0\publish\*.* .\bin\Release\netstandard2.0\publish\AvroTools\
+New-Item -Path '.\PSAvroTools\bin\Release\netstandard2.0\publish\AvroTools\' -ItemType Directory -Force
+copy .\PSAvroTools\bin\Release\netstandard2.0\publish\*.* .\PSAvroTools\bin\Release\netstandard2.0\publish\AvroTools\
 
-Publish-Module -Path .\bin\Release\netstandard2.0\publish\AvroTools -NuGetApiKey $env:NUGETAPIKEY
+Publish-Module -Path .\PSAvroTools\bin\Release\netstandard2.0\publish\AvroTools -NuGetApiKey aaa
