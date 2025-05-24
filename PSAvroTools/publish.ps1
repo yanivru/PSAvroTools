6416af763e3c6a1ab011d06d0adc757a9cf86494
yanivru <yaniv.ru@gmail.com>
2025-05-24 20:44:26 +0300

    Remove PDBs from publish

--- PSAvroTools/publish.ps1
+++ PSAvroTools/publish.ps1
@@ -1,6 +1,6 @@
 Write-Output "Starting to publish nuget package to PowerShell gallery"
 Write-Host $env:NUGETAPIKEY.Length
-dotnet publish -c Release
+dotnet publish -c Release /p:DebugType=None /p:DebugSymbols=false
 
 # Copy to AvroTools folder (Publish-Module requires that the folder will be the package name)
 New-Item -Path '.\PSAvroTools\bin\Release\netstandard2.0\publish\AvroTools\' -ItemType Directory -Force
