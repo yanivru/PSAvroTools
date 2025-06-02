0057115be4aaa3274303d7aa6fb3ce648c770e5f
Yaniv Robinpur <yrobinpur@varonis.com>
2025-06-02 20:06:25 +0300

    Fix path to avro file in integration tests

--- PSAvroTools/IntegrationTests/Read-Avro.Tests.ps1
+++ PSAvroTools/IntegrationTests/Read-Avro.Tests.ps1
@@ -1,10 +1,10 @@
 ﻿BeforeAll {
-    Import-Module .\PSAvroTools\bin\Release\netstandard2.0\publish\AvroTools\AvroTools.psd1 -Force -PassThru -Verbose
+    Import-Module ./PSAvroTools/bin/Release/netstandard2.0/publish/AvroTools/AvroTools.psd1 -Force -PassThru -Verbose
 }
 
 Describe 'Read-Avro' {
     It 'Given a valid avro file, it lists all rows' {
-        $allPlanets = Read-Avro weather.avro
+        $allPlanets = Read-Avro ./PSAvroTools/bin/Release/netstandard2.0/publish/AvroToolsweather.avro
         $allPlanets.Count | Should -Be 5
     }
 }
