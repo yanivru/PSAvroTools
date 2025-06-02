154a91e3f2710f8c860970c78efc68ad7b1b7232
Yaniv Robinpur <yrobinpur@varonis.com>
2025-06-02 20:15:11 +0300

    Oo And now the avro file name Oo

--- PSAvroTools/IntegrationTests/Read-Avro.Tests.ps1
+++ PSAvroTools/IntegrationTests/Read-Avro.Tests.ps1
@@ -4,7 +4,7 @@
 
 Describe 'Read-Avro' {
     It 'Given a valid avro file, it lists all rows' {
-        $allPlanets = Read-Avro ./PSAvroTools/IntegrationTests/AvroToolsweather.avro
+        $allPlanets = Read-Avro ./PSAvroTools/IntegrationTests/weather.avro
         $allPlanets.Count | Should -Be 5
     }
 }
